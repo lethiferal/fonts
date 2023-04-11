ddc4d0e82372a5237b6a6c80c233c209e64ada8f
Lethiferal <95504439+lethiferal@users.noreply.github.com>
2023-04-11 14:12:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -62,4 +62,6 @@ def index():
 if __name__ == '__main__':
     generate_stylesheet()
     freezer.freeze()
+    shutil.copytree('fonts', 'build/fonts', dirs_exist_ok=True)
+    print("Build Complete.")
     
